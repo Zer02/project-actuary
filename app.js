4e54b0c0f24eac5b13a3a4fa6870adcd616860ec
Anil Basdeo <abasdeo29@gmail.com>
2017-04-05 02:46:11 +0000

    added a few page tweaks

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
-    res.render("home");
+    res.redirect("videos");
 });
 
 app.get("/about", function(req, res){
